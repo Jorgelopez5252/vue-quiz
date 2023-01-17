40ce28b74989212a8fda6f3f0d44d2656fa2fa6a
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-17 01:19:25 -0700

    1 am update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,70 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref, computed} from "vue"
+
+// question block
+const questions= ref([
+  {
+    question:'What is Vue JS?',
+    answer:0,
+    options:[
+      'A front-end framwork',
+      'A library',
+      'An ice cream maker'
+    ],
+    selected:null
+  },
+  {
+    question:'What is vuex?',
+    answer:2,
+    options:[
+      'Vue with an x',
+      'A library',
+      'Stage Management Library'
+    ],
+    selected:null
+  },
+  {
+    question:'What is Vue Router used for?',
+    answer:1,
+    options:[
+      'Walking in space',
+      'Routing Library for Vue JS',
+      'A console tool option'
+    ],
+    selected:null
+  }
+  
+])
+
+const quizCompleted = ref(false)
+const currentQUestion = ref(0)
+const score =computed(() => {
+  let value = 0
+  questions.value.map(a => {
+    if(questions.selected == questions.answer) {
+      value ++
+    }
+  })
+  return value
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ <h1>Hello world</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style>
+* {
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-weight: 600;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+body {
+  background-color: #271c36;
+  color:#fff;
 }
 </style>
